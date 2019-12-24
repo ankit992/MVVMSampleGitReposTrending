33992e7d93b579e9971001aed6c6931a92649005
ankitarora <ankit992@live.in>
2019-12-24 13:30:15 +0530

    adding fragment viewmodels and nav elements

---
+++ app/src/main/java/in/co/ankitarora/mvvmsamplegithubrepos/viewModel/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package `in`.co.ankitarora.mvvmsamplegithubrepos.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/in/co/ankitarora/mvvmsamplegithubrepos/viewModel/ListViewModel.kt
@@ -0,0 +1,7 @@
+package `in`.co.ankitarora.mvvmsamplegithubrepos.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
