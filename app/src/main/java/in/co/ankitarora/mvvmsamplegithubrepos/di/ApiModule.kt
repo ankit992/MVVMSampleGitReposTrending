d3daf51dea60ca0b2be899346146d500fb68c79b
ankitarora <ankit992@live.in>
2019-12-24 23:21:08 +0530

    Adding singleton annotation

--- app/src/main/java/in/co/ankitarora/mvvmsamplegithubrepos/di/ApiModule.kt
+++ app/src/main/java/in/co/ankitarora/mvvmsamplegithubrepos/di/ApiModule.kt
@@ -7,6 +7,7 @@ import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 class ApiModule {
@@ -21,6 +22,7 @@ class ApiModule {
             .build().create(GitRepoInfoApi::class.java)
     }
 
+    @Singleton
     @Provides
     fun provideGitRepoInfoListApiService(): GitRepoInfoApiService{
         return GitRepoInfoApiService()
--- app/src/main/java/in/co/ankitarora/mvvmsamplegithubrepos/di/ViewModelComponent.kt
+++ app/src/main/java/in/co/ankitarora/mvvmsamplegithubrepos/di/ViewModelComponent.kt
@@ -2,7 +2,9 @@ package `in`.co.ankitarora.mvvmsamplegithubrepos.di
 
 import `in`.co.ankitarora.mvvmsamplegithubrepos.viewModel.ListViewModel
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [ApiModule::class])
 interface ViewModelComponent {
     fun inject(viewModel: ListViewModel)
